eb2f925f9e587889f467ab7107b182e60312624a
Bi-Bi-kubra <107239235+Bi-Bi-kubra@users.noreply.github.com>
2025-08-06 12:38:53 +0530

    Update backened.tf

--- backened.tf
+++ backened.tf
@@ -3,6 +3,7 @@ terraform {
     resource_group_name  = "dev-rg"             # Make sure this exists
     storage_account_name = "devbootcamptfstate" # Must be globally unique
     container_name       = "tfstate"            # Make sure this exists
-    # key                  = "tfstate"
+    key                  = "tfstate"
   }
 }
+
